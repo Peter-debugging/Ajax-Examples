474987a5a4de17ed8305aa06084f1bca99437e8d
Peter-debbuging <pedrohenriqueribeirodearaujo01@gmail.com>
2020-03-27 08:40:34 -0300

    Ajax

---
+++ Example2/test.php
@@ -0,0 +1,13 @@
+<?php
+
+$mysqli = new mysqli("host", "user", "password", "database");
+
+$id = $_GET["id"];
+
+$result = $mysqli->query("SELECT descricao AS msg FROM localizacao WHERE id = $id");
+
+$response = $result->fetch_assoc();
+
+date_default_timezone_set("America/Sao_Paulo");
+
+echo $response["msg"] . date("H:i:s");
---
+++ Example3/server-side.php
@@ -0,0 +1,12 @@
+<?php
+$mysqli = new mysqli("host", "user", "password", "database");
+
+$id = $_POST["id"];
+
+$result = $mysqli->query("SELECT descricao AS msg FROM localizacao WHERE id = $id");
+
+$response = $result->fetch_assoc();
+
+date_default_timezone_set("America/Sao_Paulo");
+
+echo $response["msg"] . ' ' . date("H:i:s");
